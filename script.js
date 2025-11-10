9447e4f8450a2f06af455e6ad614c898343288fd
Agus <muh.baguswidiyanto17@gmail.com>
2025-11-11 00:26:29 +0800

    done

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ function toggleSidebar() {
         openSidebar();
     }
 }
+
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        tabs.forEach((t) => t.classList.remove("active"));
+        contents.forEach((c) => c.classList.remove("active"));
+
+        tab.classList.add("active");
+        document.getElementById(tab.dataset.tab).classList.add("active");
+    });
+});
+
+/* 1. [DIMODIFIKASI] Pilih elemen yang kita butuhkan
+Kita sekarang memilih gambar, BUKAN lagi div .profile-area */
+const profileButton=document.getElementById('profilePictureButton');
+const profileMenu=document.getElementById('profileMenu');
+
+/* 2. [DIMODIFIKASI] Tambahkan event 'click' ke tombol GAMBAR */
+profileButton.addEventListener('click', function (event) {
+        profileMenu.classList.toggle('show');
+        event.stopPropagation();
+    });
+
+/* 3. (Sama) Menutup dropdown jika klik di luar area menu */
+window.addEventListener('click', function (event) {
+        if (profileMenu.classList.contains('show')) {
+
+            // Cek apakah klik BUKAN di dalam menu DAN BUKAN di tombol gambar
+            if ( !profileMenu.contains(event.target) && !profileButton.contains(event.target)) {
+                profileMenu.classList.remove('show');
+            }
+        }
+    });
